78fd1c12231776780e1fdeea1d5733a8ceb2a96b
Ahmed Samir <ahmedsamir7685@gmail.com>
2025-09-18 10:42:28 +0300

    deploy

--- src/layout/Brands.jsx
+++ src/layout/Brands.jsx
@@ -2,12 +2,10 @@
 const Brands = () => {
     return (
         <section className='flex flex-wrap md:flex-nowrap justify-between w-full h-40 items-start lg:px-[80px] md:px-[30px] px-[10px] py-[24px] gap-10'>
-            <img className='md:w-1/12 w-2/12 h-8' src="/Google.png" alt="Google" />
-            <img className='md:w-1/12 w-2/12 h-8' src="/Microsoft.png" alt="Microsoft" />
-            <img className='md:w-1/12 w-2/12 h-8' src="/Spotify.png" alt="Spotify" />
-            <img className='md:w-1/12 w-2/12 h-8' src="/MailChimp.png" alt="MailChimp" />
-            <img className='md:w-1/12 w-2/12 h-8' src="/Airbnb.png" alt="Airbnb" />
-            <img className='md:w-1/12 w-2/12 h-8' src="/Uber.png" alt="aber" />
+            <img className='md:w-1/12 w-3/12 h-8' src="/Google.png" alt="Google" />
+            <img className='md:w-1/12 w-3/12 h-8' src="/Microsoft.png" alt="Microsoft" />
+            <img className='md:w-1/12 w-3/12 h-8' src="/Spotify.png" alt="Spotify" />
+            <img className='md:w-1/12 w-3/12 h-8' src="/Airbnb.png" alt="Airbnb" />
         </section>
     )
 }
