2c15acb994afbde9b9c6f35c78e696362f6f6e59
Ahmed Samir <ahmedsamir7685@gmail.com>
2025-09-18 10:08:54 +0300

    deploy

--- src/layout/Brands.jsx
+++ src/layout/Brands.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 
 const Brands = () => {
     return (
         <section className='flex flex-wrap md:flex-nowrap justify-between w-full h-40 items-start lg:px-[80px] md:px-[30px] px-[10px] py-[24px] gap-10'>
-            <img className='md:w-1/12 w-2/12 h-8' src="../../public/Google.png" alt="Google" />
-            <img className='md:w-1/12 w-2/12 h-8' src="../../public/Microsoft.png" alt="Microsoft" />
-            <img className='md:w-1/12 w-2/12 h-8' src="../../public/Spotify.png" alt="Spotify" />
-            <img className='md:w-1/12 w-2/12 h-8' src="../../public/MailChimp.png" alt="MailChimp" />
-            <img className='md:w-1/12 w-2/12 h-8' src="../../public/Airbnb.png" alt="Airbnb" />
-            <img className='md:w-1/12 w-2/12 h-8' src="../../public/Uber.png" alt="Uber" />
+            <img className='md:w-1/12 w-2/12 h-8' src="/Google.png" alt="Google" />
+            <img className='md:w-1/12 w-2/12 h-8' src="/Microsoft.png" alt="Microsoft" />
+            <img className='md:w-1/12 w-2/12 h-8' src="/Spotify.png" alt="Spotify" />
+            <img className='md:w-1/12 w-2/12 h-8' src="/MailChimp.png" alt="MailChimp" />
+            <img className='md:w-1/12 w-2/12 h-8' src="/Airbnb.png" alt="Airbnb" />
+            <img className='md:w-1/12 w-2/12 h-8' src="/Uber.png" alt="Uber" />
         </section>
     )
 }
