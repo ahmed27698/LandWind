9e1f51a0c75f73f47c174b11d5a3a90a495492ef
Ahmed Samir <ahmedsamir7685@gmail.com>
2025-09-18 10:02:12 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Nav from "./layout/nav"
 import Company from "./layout/Company"
-import Brands from "./layout/brands"
+import Brands from "./layout/Brands"
 import Features from "./layout/Features"
 import FeaturesTwo from "./layout/FeaturesTwo"
 import MarketPlace from "./layout/MarketPlace"
-import Team from "./layout/team"
+import Team from "./layout/Team"
 import Contact from "./layout/Contact"
 import Footer from "./layout/Footer"
 export default function App() {
